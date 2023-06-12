50677ee5a166e56eb1bf656d66c56e9a45dbd048
Dovid Yarmush <yarmushda@gmail.com>
2023-06-12 16:03:13 -0400

    passing test fibonacci sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_list = [0, 1]
+    if length == 0:
+       print([])
+    elif length == 1:
+        print([0])
+    elif length == len(fib_list):    
+        print(fib_list)
+    else:
+        for i in range(1, length-1):
+            fib_list.append(fib_list[i] + fib_list[i - 1])
+        print(fib_list)
+
+
+print_fibonacci(100)
